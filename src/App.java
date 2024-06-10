b18b2bb8d96b300df47b518720c4ecd5c4d714e9
Owen Powell <owen.powell2@student.tdsb.on.ca>
2024-06-09 21:31:06 -0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,21 @@
-public class App {
-    public static void main (String[] args) throws Exception {
-        
-    }
-    }
+import javax.swing.JOptionPane;
 
+public class AverageCalculator {
+
+    public static void main(String[] args) {
+        // Calculate the average
+        String firstValueStr = JOptionPane.showInputDialog(null, "Enter the first value:");
+        double firstValue = Double.parseDouble(firstValueStr);
+
+        String secondValueStr = JOptionPane.showInputDialog(null, "Enter the second value:");
+        double secondValue = Double.parseDouble(secondValueStr);
+
+        String thirdValueStr = JOptionPane.showInputDialog(null, "Enter the third value:");
+        double thirdValue = Double.parseDouble(thirdValueStr);
+
+        double average = (firstValue + secondValue + thirdValue) / 3;
+
+        // Display the average
+        JOptionPane.showMessageDialog(null, "The average of the three numbers is: " + average);
+    }
+}
